312402a86516c58f4317c724b62325dd58f77233
Vadim <61653819+Exqzore@users.noreply.github.com>
2021-02-16 14:51:21 +0300

    Delete src directory

--- src/com/exqzor/task/task7/Calculator.java
+++
@@ -1,7 +0,0 @@
-package com.exqzor.task.task7;
-
-public class Calculator {
-    public static int calculateSumOfSquares(int x, int y) {
-        return (int) (Math.pow(x, 2) + Math.pow(y, 2));
-    }
-}
